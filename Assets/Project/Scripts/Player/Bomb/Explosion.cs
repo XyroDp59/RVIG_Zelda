359a2a964b44b3ef91378c6fbb1d908eeb96b146
XyroDp59 <81575508+XyroDp59@users.noreply.github.com>
2025-03-02 18:10:09 +0100

    Update prefab et début de travail sur les armures

---
+++ Assets/Project/Scripts/Player/Bomb/Explosion.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        /* 
+        TODO :
+        Armor armor;
+         
+        if(other.TryGetComponent(out armor))
+        {
+            armor.Break();
+        }
+          
+         */
+    }
+}

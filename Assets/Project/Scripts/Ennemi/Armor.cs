c84d56afc223b4be48aea22d9797c8b0bd04f5b4
Wangha <132218157+Arwangha@users.noreply.github.com>
2025-03-22 23:56:46 +0100

    Fix armored skeletons

--- Assets/Project/Scripts/Ennemi/Armor.cs
+++ Assets/Project/Scripts/Ennemi/Armor.cs
@@ -1,12 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Armor : MonoBehaviour
 {
+    private Vector3 _defaultLocalPosition;
+    
+    private void Start()
+    {
+        _defaultLocalPosition = transform.localPosition;
+    }
+
     public void Break()
     {
         gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        transform.localPosition = _defaultLocalPosition;
     }
 
     private void OnCollisionEnter(Collision collision)

96582f0f2d4f50a382ddba33f30c5ecd5eda2ea7
idimension <41388029+idimension18@users.noreply.github.com>
2025-03-23 20:14:38 +0100

    Add brumble on damage

--- Assets/Project/Scripts/Player/Player.cs
+++ Assets/Project/Scripts/Player/Player.cs
@@ -1,17 +1,31 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit.Inputs.Haptics;
 
 public class Player : MonoBehaviour
 {
     private Health health;
+    
+    private HapticImpulsePlayer _hapticImpulseLeft;
+    private HapticImpulsePlayer _hapticImpulseRight;
 
     private void Start()
     {
         health = GetComponent<Health>();
         health.Damaged.AddListener(OnDamage);
+        
+        _hapticImpulseLeft = XRData.Instance.leftHand.GetComponent<HapticImpulsePlayer>();
+        _hapticImpulseRight = XRData.Instance.rightHand.GetComponent<HapticImpulsePlayer>();
     }
+    
+    
 
     private void OnDamage()
     {
         StartCoroutine(PostProcessControl.Instance.DamageVignetteFlicker());
+
+        _hapticImpulseLeft.SendHapticImpulse(1, 0.5f);
+        _hapticImpulseRight.SendHapticImpulse(1, 0.5f);
     }
 }
